952aea6dabcab8f4cf6d1f855e06cf2f11db0f72
IsrafilMatter <148543830+IsrafilMatter@users.noreply.github.com>
2025-03-06 20:47:51 +0800

    Update Prog05.py

--- Prog05.py
+++ Prog05.py
@@ -1,7 +1,7 @@
 # Prog05: Print the quotient of the two numbers with the decimal point
 num1 = float(input('Enter the first number: '))
 num2 = float(input('Enter the second number: '))
-if num2 != 0:
+if num1 != 0 and num2 != 0:
     print(f'The quotient is: {num1 / num2}')
 else:
     print('Cannot divide by zero')
